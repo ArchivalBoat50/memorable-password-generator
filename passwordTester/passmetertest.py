1f4622e9d66d63a7e247ad2e3c54f1deb959663f
Ali Siddiqui <fudgecoated@gmail.com>
2021-06-15 20:04:46 -0600

    added more comments

--- passwordTester/passmetertest.py
+++ passwordTester/passmetertest.py
@@ -1,14 +1,14 @@
 import re
 
-lengthLevel = None
-charLevel = None
+lengthLevel = None                  #Gives relative value for password length (1-  <8, 2-  >=8 & <12, 3- >=12)
+charLevel = None                    #Gives relative value for character 'complexity' (1- upper and lower case, 2- upper or lowercase, 3- no letters)
 includesDigits = None
 lowerRegex = re.compile(r'[a-z]+')      # Regex function for finding lowercase letters
 upperRegex = re.compile(r'[A-Z]+')      # Regex function for finding uppercase letters
 digitRegex = re.compile(r'[0-9]+')      # Regex function for finding digits
 
 def passwordLength (password):
-
+# Defines relative password length
     if passwordText.len() < 8:
         lengthLevel = 0
     elif passwordText.len() >= 8 and passwordText.len() < 12:
@@ -18,6 +18,7 @@ def passwordLength (password):
 
 
 def characterCases (password):
+#Defines relative password inclusion of letters
     if upperRegex.findall(password) and lowerRegex.findall(password):
         charLevel = 2
     elif upperRegex.findall(password) or lowerRegex.findall(password):
@@ -27,6 +28,7 @@ def characterCases (password):
         charLevel =0
 
 def hasDigits (password):
+# Defines if password contains digits
     if digitRegex.findall(password):
         includesDigits = True
     elif
