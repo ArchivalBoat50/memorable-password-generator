0fcee306b47d8e136ed8d4410b7bf23654a994af
ArchivalBoat50 <8140erich@gmail.com>
2021-06-15 22:54:05 -0600

    added passSimple() passDigits()

--- passwordTester/passMeterTest_defs.py
+++ passwordTester/passMeterTest_defs.py
@@ -1,4 +1,4 @@
-import regex 
+import re 
 # Settings
 BAR_LESS_VERY_LOW = 10
 BAR_LESS_LOW = 50
@@ -6,7 +6,7 @@ BAR_LESS_MED = 100
 BAR_LESS_STR = 200
 
 # Debug
-debug_mod = True
+debug_mod = False
 
 # CONST
 NUM_CHAR_MULT = 4
@@ -26,21 +26,41 @@ def passUpper(password):
     num_of_lower = sum(1 for c in password if c.islower())
     if debug_mod:
           print((len(password)-num_of_lower)*NUM_CHAR_MULT/2)
-    return ((len(password)-num_of_lower)*NUM_CHAR_MULT/2)
+    return (len(password)-num_of_lower)*NUM_CHAR_MULT/2
 
 def passDigits(password):
-    num = 0
-    return num
+    num_of_digits = sum(1 for c in password if c.isdigit())
+    if debug_mod:
+        print((len(password)-num_of_digits)*NUM_CHAR_MULT/2)
+    return (len(password)-num_of_digits)*NUM_CHAR_MULT/2
 
 def passSymbols(password):
     num_of_symbol = len(re.sub('[\w]+' ,'', password))
     if debug_mod:
         print((len(password)-num_of_symbol)*NUM_CHAR_MULT/2)
-    return len(password)-num_of_symbol)*NUM_CHAR_MULT/2
+    return (len(password)-num_of_symbol)*NUM_CHAR_MULT/2
+
+def containsSymbol(password):
+   re.findall('[^A-Za-z0-9]',password)
+   if (re.findall('[^A-Za-z0-9]',password)):
+     return True
+   else:
+     return False 
 
 def passSimple(password):
-    num = 0
-    return num
+    simpleIndex = 0
+    containsUpper = bool(re.match(r'\w*[A-Z]\w*', password))
+    containsLower = bool(re.match(r'\w*[a-z]\w*', password))
+    containsDigit = any(char.isdigit() for char in password)
+    if not(containsUpper):
+        simpleIndex += 1
+    if not(containsLower):
+        simpleIndex += 1
+    if not(containsDigit):
+        simpleIndex += 1
+    if not(containsSymbol(password)):
+        simpleIndex += 1
+    return NUM_CHAR_MULT*simpleIndex
         
 
 def passwordStrength(password):
