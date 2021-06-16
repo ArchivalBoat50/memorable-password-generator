929e820258ff7bfbf9af0e1751da54dde4ad68a0
ArchivalBoat50 <8140erich@gmail.com>
2021-06-15 18:35:46 -0600

    Made a better demonstration of code

--- passwordGeneration.py
+++ passwordGeneration.py
@@ -1,20 +1,19 @@
-import os, random, string
+# Example sentence list
+list1 = ["The", "Quick", "Brown", "Fox", "Jumped", "Above", "The", "Fence"]
 
-
-
-list1 = ["The", "Quick", "Brown", "Fox", "and"]
-
-replacements = ( ('a', '@'), ('T', '7'))
-# We can use a random  mneumonic sentence generator instead of having a user manually put it in
+replacements2 = (('A', '@'), ('B', '8'))
+replacements3 = ( ('a', '@'), ('T', '7'), ('A', '@'), ('B', '8'), ('F', 'Ph'))
+# The higher the complexity, the more possible character replacements there are for the mnemonic password
 def mneumonicPass(wordList, complexity):
     letters = [s[0] for s in wordList]
     password = "".join(letters)
 
     if (complexity == 2):
-        print("a")
+        for old, new in replacements2:
+            password = password.replace(old, new)
         return password
     elif (complexity == 3):
-        for old, new in replacements:
+        for old, new in replacements3:
             password = password.replace(old, new)
         return password
     else:
@@ -22,7 +21,11 @@ def mneumonicPass(wordList, complexity):
 
 
 def main():
-    print(mneumonicPass(list1, 2))
+    print("Complexity of 1 \n")
+    print(mneumonicPass(list1, 1))
+    print("Complexity of 2 \n")
+    print(mneumonicPass(list1, 2), "\n")
+    print("Complexity of 3 \n")
     print(mneumonicPass(list1, 3))
 
 main()
